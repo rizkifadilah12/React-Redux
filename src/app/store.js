dc78288477a3396ecaba9973093be2cfa90204c9
rizkifadilah12 <rf8740072@gmail.com>
2021-12-21 09:42:07 +0700

    first commit

---
+++ src/app/store.js
@@ -0,0 +1,7 @@
+import { configureStore } from '@reduxjs/toolkit';
+import productReducer from "../features/ProductSlice";
+export const store = configureStore({
+  reducer: {
+   product: productReducer
+  },
+});
